71dd12450c8bc639acf04f21e1a7a3ae5f9ebabe
byTimo <race.timo@gmail.com>
2020-02-18 22:41:05 +0500

    append range and repeat

--- src/index.test.ts
+++ src/index.test.ts
@@ -7,17 +7,49 @@ function typeGuard(a: number | string): a is number {
 describe("chain", () => {
     describe("chain global", () => {
         //TODO (byTimo) Set, Map, Record
+        describe("range", () => {
+            it("start from zero", () => {
+                const actual = chain.range(0, 5).array;
+                expect(actual).toEqual([0, 1, 2, 3, 4]);
+            });
+            it("start from 5", () => {
+                const actual = chain.range(5, 5).array;
+                expect(actual).toEqual([5, 6, 7, 8, 9]);
+            });
+            it("count is zero", () => {
+                const actual = chain.range(5, 0).array;
+                expect(actual).toEqual([]);
+            });
+            it("count less then zero", () => {
+                const actual = chain.range(5, -1).array;
+                expect(actual).toEqual([]);
+            })
+        });
+        describe("repeat", () => {
+            it("repeat number", () => {
+                const actual = chain.repate(10, 5).array;
+                expect(actual).toEqual([10, 10, 10, 10, 10]);
+            });
+            it("repeat object", () => {
+                const obj = { a: 10 };
+                const actual = chain.repate(obj, 3).array;
+                expect(actual).toEqual([obj, obj, obj]);
+                expect(actual[0]).toBe(obj);
+                expect(actual[1]).toBe(obj);
+                expect(actual[2]).toBe(obj);
+            })
+        })
         it("map", () => {
-            const actual = chain.map([1, 2, 3, 4], x => x + 50).toArray();
+            const actual = chain.map([1, 2, 3, 4], x => x + 50).array;
             expect(actual).toEqual([51, 52, 53, 54]);
         });
         describe("filter", () => {
             it("array - condition return boolean", () => {
-                const actual = chain.filter([1, 2, 3, 4], x => x % 2 == 0).toArray()
+                const actual = chain.filter([1, 2, 3, 4], x => x % 2 == 0).array
                 expect(actual).toEqual([2, 4])
             })
             it("array - type guard", () => {
-                const actual = chain.filter([1, "2", 3, "4"], typeGuard).toArray();
+                const actual = chain.filter([1, "2", 3, "4"], typeGuard).array;
                 const first: number = actual[0] //type check
                 expect(actual).toEqual([1, 3]);
             })
@@ -25,11 +57,11 @@ describe("chain", () => {
     })
     describe("chain", () => {
         it("1", () => {
-            const actual = chain([1, 2, 3, 4]).map(x => x + 50).toArray();
+            const actual = chain([1, 2, 3, 4]).map(x => x + 50).array;
             expect(actual).toEqual([51, 52, 53, 54])
         });
         it("2", () => {
-            const actual = chain(["1", "2"]).map(x => parseInt(x)).toArray();
+            const actual = chain(["1", "2"]).map(x => parseInt(x)).array;
             expect(actual).toEqual([1, 2]);
         })
     })
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 export const chain = (function () {
     const create = function <T>(source: Iterable<T>) {
-        return new ChainIterator(source);
+        return new ChainIterable(source);
     }
+    create.range = range;
+    create.repate = repeat;
     create.map = map;
     create.filter = filter;
     return create;
@@ -9,22 +11,46 @@ export const chain = (function () {
 
 export type Chain = typeof chain;
 
-class ChainIterator<T> {
+class ChainIterable<T> {
     constructor(private readonly source: Iterable<T>) {
     }
 
-    map = <R>(selector: (item: T, index: number) => R): ChainIterator<R> => {
+    get iterable() {
+        return this.source;
+    }
+
+    map = <R>(selector: (item: T, index: number) => R): ChainIterable<R> => {
         return map(this.source, selector);
     }
 
-    toArray = (): T[] => {
+    get array(): T[] {
         return Array.from(this.source);
+    }
+}
+
+function range(start: number, count: number) {
+    return new ChainIterable(rangeGenerator(start, count));
+}
+
+function* rangeGenerator(start: number, count: number) {
+    for (let i = 0; i < count; i++) {
+        yield start + i;
+    }
+}
+
+function repeat<T>(value: T, count: number) {
+    return new ChainIterable(repeatGenerator(value, count));
+}
+
+function* repeatGenerator<T>(value: T, count: number) {
+    for (let i = 0; i < count; i++) {
+        yield value;
     }
 }
 
 
 function map<T, R>(source: Iterable<T>, selector: (item: T, index: number) => R) {
-    return new ChainIterator(mapGenerator(source, selector));
+    return new ChainIterable(mapGenerator(source, selector));
 }
 
 function* mapGenerator<T, R>(source: Iterable<T>, selector: (item: T, index: number) => R) {
@@ -35,10 +61,10 @@ function* mapGenerator<T, R>(source: Iterable<T>, selector: (item: T, index: num
     }
 }
 
-function filter<T, S extends T>(source: Iterable<T>, condition: (item: T, index: number) => item is S): ChainIterator<S>
-function filter<T>(source: Iterable<T>, condition: (item: T, index: number) => boolean): ChainIterator<T>
+function filter<T, S extends T>(source: Iterable<T>, condition: (item: T, index: number) => item is S): ChainIterable<S>
+function filter<T>(source: Iterable<T>, condition: (item: T, index: number) => boolean): ChainIterable<T>
 function filter<T>(source: Iterable<T>, condition: (item: T, index: number) => boolean) {
-    return new ChainIterator(filterGenerator(source, condition));
+    return new ChainIterable(filterGenerator(source, condition));
 }
 
 function* filterGenerator<T>(source: Iterable<T>, condition: (item: T, index: number) => boolean) {
